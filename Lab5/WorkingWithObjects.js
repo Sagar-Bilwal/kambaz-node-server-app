5f52d657fdc63ece65a624cdea1143cfe34f300e
Sagar-Bilwal <bilwal.s@northeastern.edu>
2025-04-01 20:01:35 -0400

    Completed till 5.2.5.5

--- Lab5/WorkingWithObjects.js
+++ Lab5/WorkingWithObjects.js
@@ -3,14 +3,55 @@ const assignment = {
     description: "Create a NodeJS server with ExpressJS",
     due: "2021-10-10", completed: false, score: 0,
   };
+const module = {
+  id: "1", name: "Test Module", description: "This is a test module to learn Node js",
+  course: "Learn Node js"
+}
   export default function WorkingWithObjects(app) {
-    app.get("/lab5/assignment", (req, res) => {
+    app.get("/lab5/assignment", (_, res) => {
       res.json(assignment);
     });
-    app.get("/lab5/assignment/title", (req, res) => {
+    app.get("/lab5/assignment/title", (_, res) => {
         res.json(assignment.title);
-      });
+    });
+
+    app.get("/lab5/assignment/title/:title", (req, res) => {
+      const {title} = req.params
+      assignment.title = title
+      res.json(assignment);
+    });
+
+    app.get("/lab5/module", (_, res) => {
+      res.json(module);
+    });
+
+    app.get("/lab5/module/name", (_, res) => {
+      res.json(module.name);
+    });
     
+    app.get("/lab5/module/update/name", (req, res) => {
+      const { name } = req.query;
+      module.name = name;
+      res.send("Module Name updated successfully");
+    });
+
+    app.get("/lab5/assignment/update/score", (req, res) => {
+      const { score } = req.query;
+      assignment.score = score;
+      res.send("Assignment Score updated successfully");
+    });
+
+    app.get("/lab5/assignment/update/completed", (req, res) => {
       
-};
+      const { completed } = req.query;
+      assignment.completed = completed;
+      res.send("Assignment Status updated successfully");
+    });
+
+    app.get("/lab5/module/update/description", (req, res) => {
+      const { description } = req.query;
+      module.description = description;
+      res.send("Module Description updated successfully");
+    });
+  };
   
--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import Lab5 from "./Lab5/index.js";
 import PathParameters from './Lab5/PathParameters.js';
 import QueryParameters from './Lab5/QueryParameters.js';
 import WorkingWithObjects from './Lab5/WorkingWithObjects.js';
-const app = express()
+import WorkingWithArrays from './Lab5/WorkingWithArrays.js';
+import cors from "cors";
+const app = express();
+app.use(cors());
 Hello(app);
 Lab5(app);
 PathParameters(app);
 QueryParameters(app);
 WorkingWithObjects(app);
+WorkingWithArrays(app);
 app.listen(process.env.PORT || 4000)
